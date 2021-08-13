6ba900f0af1449a8cff5630064686ef9c3c8a4b7
IvanGerman <ivgvi@mail.ru>
2021-08-14 02:08:15 +0700

    feat: implement vanilla-tilt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,16 @@ import Stats from './components/Stats/Stats';
 import Topics from './components/Topics/Topics';
 
 function App() {
+
+  const options = {
+    max: 17,
+    speed: 800,
+    glare: true,
+    "max-glare": 1
+  };
   return (
     <div className="app-wrapper">
-      <Route exact path = '/' component = {StartPage} />
+      <Route exact path = '/' render = { () => <StartPage options={options} /> } />  
       <Route exact path = '/topics' component = {Topics} />
       <Route exact path = '/stats' component = {Stats} />
     </div>
--- frontend/src/components/StartPage/StartPage.jsx
+++ frontend/src/components/StartPage/StartPage.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './StartPage.module.css';
 //import { NavLink } from 'react-router-dom';
+import VanillaTilt from 'vanilla-tilt';
 
 const StartPage = (props) => {  
 
+  const { options } = props;
+  const tilt = useRef(null);
+
+  useEffect(() => {
+    VanillaTilt.init(tilt.current, options);
+  }, [options]);
+
   return (
-    <div  className = { styles.wrapper }>
-      <div  className = { styles.container }>
-        <div  className = { styles.card } id = { styles.appName }>
+    <div  className = { styles.wrapper }  >
+      <div  className = { styles.container } >
+        <div  className = { styles.card } id = { styles.appName } ref={tilt}>
           <div  className = { styles.content } >
             <h3 id="main-h3">KnowledgeSource</h3>
             <br />
             <h4>Your helper by studying and memorizing new learning materials</h4>
           </div>
         </div>
-        <div className = { styles.card } id = { styles.topics }>
+        <div className = { styles.card } id = { styles.topics }  >
           <div  className = { styles.content } >
             <h3>Topics</h3>
             <br />
@@ -22,7 +30,7 @@ const StartPage = (props) => {
             <a href="#">Get Started</a>
           </div>
         </div>
-        <div className = { styles.card } id = { styles.stats }>
+        <div className = { styles.card } id = { styles.stats }  >
           <div className = { styles.content } >
             <h3>Stats</h3>
             <br />
