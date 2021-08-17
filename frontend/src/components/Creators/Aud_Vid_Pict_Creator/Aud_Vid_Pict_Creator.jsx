4e5c615c0d4e241584656657fbde350f0a9f07a5
IvanGerman <ivgvi@mail.ru>
2021-08-17 23:35:49 +0700

    feat: add Aud_Vid_Pict_Creator to Creators direction

---
+++ frontend/src/components/Creators/Aud_Vid_Pict_Creator/Aud_Vid_Pict_Creator.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Aud_Vid_Pict_Creator.module.css';
+//import { NavLink } from 'react-router-dom';
+
+const Aud_Vid_Pict_Creator = (props) => {  
+
+  return (
+    <div  className = { styles.wrapper }>
+      Aud_Vid_Pict_Creator
+    </div>
+  )
+};
+
+export default Aud_Vid_Pict_Creator;
