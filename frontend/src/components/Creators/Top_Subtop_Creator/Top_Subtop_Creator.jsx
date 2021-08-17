ac22406812d0dec2e61f9e20029d9b86d1e37c15
IvanGerman <ivgvi@mail.ru>
2021-08-17 23:30:59 +0700

    feat: add Top_Subtop_Creator to Creators direction

---
+++ frontend/src/components/Creators/Top_Subtop_Creator/Top_Subtop_Creator.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Topic.module.css';
+//import { NavLink } from 'react-router-dom';
+
+const Topic = (props) => {  
+
+  return (
+    <div  className = { styles.wrapper }>
+      Single Topic
+    </div>
+  )
+};
+
+export default Topic;
