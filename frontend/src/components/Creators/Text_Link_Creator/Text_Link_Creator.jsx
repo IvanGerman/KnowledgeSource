796799e3e206bfee66bf19508fb1cdfe61abf62d
IvanGerman <ivgvi@mail.ru>
2021-08-17 23:33:46 +0700

    feat: add Text_Link_Creator to Creators direction

---
+++ frontend/src/components/Creators/Text_Link_Creator/Text_Link_Creator.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Text_Link_Creator.module.css';
+//import { NavLink } from 'react-router-dom';
+
+const Text_Link_Creator = (props) => {  
+
+  return (
+    <div  className = { styles.wrapper }>
+      Text_Link_Creator
+    </div>
+  )
+};
+
+export default Text_Link_Creator;
