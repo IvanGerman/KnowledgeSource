279fec4ddade257080f1a754800b7aa23ec066b3
IvanGerman <ivgvi@mail.ru>
2021-08-22 22:21:41 +0700

    feat: add ContentBlocks directory

---
+++ frontend/src/components/ContentBlocks/ContentBlocks.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './ContentBlocks.module.css';
+//import { NavLink } from 'react-router-dom';
+
+const ContentBlocks = (props) => {  
+
+  return (
+    <div  className = { styles.wrapper }>
+      ContentBlocks
+    </div>
+  )
+};
+
+export default ContentBlocks;
