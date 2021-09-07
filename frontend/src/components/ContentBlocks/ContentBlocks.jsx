7780de5d74d0bda0d55278a92482f6147dfc0c7a
IvanGerman <ivgvi@mail.ru>
2021-09-07 09:18:23 +0700

    feat: add switch statement to maping of ContentBlocks

--- frontend/src/components/ContentBlocks/ContentBlocks.jsx
+++ frontend/src/components/ContentBlocks/ContentBlocks.jsx
@@ -9,24 +9,71 @@ import VideoBlock from './VideoBlock/VideoBlock';
 
 const ContentBlocks = (props) => {  
 
-  const arrForMap = props.contentBlocks.filter(elem => elem.subtopicId === props.idForMaping);;
+  const arrForMap = props.contentBlocks.filter(elem => elem.subtopicId === props.idForMaping);
 
-  // const allContentBlocks = arrForMap.map((t, i) =>
+  const allContentBlocks = arrForMap.map((elem, i) => { 
 
-  //   <SubTopicContainer id={arrForMap[i].id} name={arrForMap[i].name}  
-  //                     creationDate={arrForMap[i].creationDate}
-  //                     lastVisitDate={arrForMap[i].lastVisitDate}
-  //                     views={arrForMap[i].views} contentBlocksNumber={arrForMap[i].contentBlocksNumber}
-  //                     key={i} />);
+    switch (elem.kindOfBlock) {
 
+      case 'text':
+        return <Text id={arrForMap[i].id} kindOfBlock={arrForMap[i].kindOfBlock} 
+              subtopicId={arrForMap[i].subtopicId} content={arrForMap[i].content}
+              commentInfo={arrForMap[i].commentInfo} creationDate={arrForMap[i].creationDate}
+              lastVisitDate={arrForMap[i].lastVisitDate} views={arrForMap[i].views}
+              memoModeOn={arrForMap[i].memoModeOn} selectionTime={arrForMap[i].selectionTime}
+              repeatLevel={arrForMap[i].repeatLevel} countdownColor={arrForMap[i].countdownColor}
+              key={i} />;
+
+      case 'link':
+        return <Link id={arrForMap[i].id} kindOfBlock={arrForMap[i].kindOfBlock}
+              subtopicId={arrForMap[i].subtopicId} content={arrForMap[i].content}
+              commentInfo={arrForMap[i].commentInfo} creationDate={arrForMap[i].creationDate}
+              lastVisitDate={arrForMap[i].lastVisitDate} views={arrForMap[i].views}
+              memoModeOn={arrForMap[i].memoModeOn} selectionTime={arrForMap[i].selectionTime}
+              repeatLevel={arrForMap[i].repeatLevel} countdownColor={arrForMap[i].countdownColor}
+              key={i} />;
+
+      case 'picture':
+        return <Picture id={arrForMap[i].id} kindOfBlock={arrForMap[i].kindOfBlock}
+                subtopicId={arrForMap[i].subtopicId} content={arrForMap[i].content}
+                commentInfo={arrForMap[i].commentInfo} creationDate={arrForMap[i].creationDate}
+                lastVisitDate={arrForMap[i].lastVisitDate} views={arrForMap[i].views}
+                memoModeOn={arrForMap[i].memoModeOn} selectionTime={arrForMap[i].selectionTime}
+                repeatLevel={arrForMap[i].repeatLevel} countdownColor={arrForMap[i].countdownColor}
+                key={i} />;
+
+      case 'audio':
+        return <AudioBlock id={arrForMap[i].id} kindOfBlock={arrForMap[i].kindOfBlock}
+                subtopicId={arrForMap[i].subtopicId} content={arrForMap[i].content}
+                commentInfo={arrForMap[i].commentInfo} creationDate={arrForMap[i].creationDate}
+                lastVisitDate={arrForMap[i].lastVisitDate} views={arrForMap[i].views}
+                memoModeOn={arrForMap[i].memoModeOn} selectionTime={arrForMap[i].selectionTime}
+                repeatLevel={arrForMap[i].repeatLevel} countdownColor={arrForMap[i].countdownColor}
+                key={i} />;
+
+      case 'video':
+        return <VideoBlock id={arrForMap[i].id} kindOfBlock={arrForMap[i].kindOfBlock}
+                subtopicId={arrForMap[i].subtopicId} content={arrForMap[i].content}
+                commentInfo={arrForMap[i].commentInfo} creationDate={arrForMap[i].creationDate}
+                lastVisitDate={arrForMap[i].lastVisitDate} views={arrForMap[i].views}
+                memoModeOn={arrForMap[i].memoModeOn} selectionTime={arrForMap[i].selectionTime}
+                repeatLevel={arrForMap[i].repeatLevel} countdownColor={arrForMap[i].countdownColor}
+                key={i}/>;
+
+      default:
+        return null;
+    }
+  });
+  
   return (
     <div className={styles.wrapper}>
       <div className={styles.allContentBlocks}>
-        <Text />
+        {allContentBlocks}
+        {/* <Text />
         <Link />
         <Picture />
         <AudioBlock />
-        <VideoBlock />
+        <VideoBlock /> */}
       </div>
       <div className={styles.backButtonOuter}>
         <BackButton />
